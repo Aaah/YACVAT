00e7a0ff8717263f893af0bdec9302d570c03f86
Rémy VINCENT <rmy.vincent@gmail.com>
2022-11-26 20:50:27 +0100

    update gitignore

--- include/yacvat/version.h
+++ include/yacvat/version.h
@@ -3,6 +3,6 @@
 
 #define YACVAT_VER_MAJOR (1)
 #define YACVAT_VER_MINOR (2)
-#define YACVAT_VER_PATCH (21) // obtained via : git rev-list HEAD --count or git rev-list TAGNAME..HEAD --count
+#define YACVAT_VER_PATCH (25) // obtained via : git rev-list HEAD --count or git rev-list TAGNAME..HEAD --count
 
 #endif
